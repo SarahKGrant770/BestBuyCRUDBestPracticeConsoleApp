cbd14d4148dcba84a937b876cac7cd5eebb7953f
GrantS <sarahkathleengrant@gmail.com>
2020-07-23 09:48:45 -0600

    Add project files.

---
+++ BestBuyCRUDBestPracticeConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestBuyCRUDBestPracticeConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
